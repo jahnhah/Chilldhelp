16a4f97fce26bc669bddf162693316096a3117a1
jahnhah <jahnhahcraven@gmail.com>
2022-05-12 13:01:03 +0300

    Revert "Linking Login Activity to service"
    This reverts commit de0c68a8407baca34eff3e33ea68d81aaacad857.

--- app/src/main/java/com/jahnhahcraven/childhelp/service/BaseService.java
+++ app/src/main/java/com/jahnhahcraven/childhelp/service/BaseService.java
@@ -6,18 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class BaseService {
     private static BaseService instance=new BaseService();
     public IPostService postService;
-<<<<<<< Updated upstream
-=======
-    public IUserService userService;
->>>>>>> Stashed changes
 
     private BaseService(){
         Retrofit retrofit= new Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com").addConverterFactory(GsonConverterFactory.create()).build();
         postService=retrofit.create(IPostService.class);
-<<<<<<< Updated upstream
-=======
-        userService=retrofit.create(IUserService.class);
->>>>>>> Stashed changes
     }
 
     public static BaseService getInstance(){
